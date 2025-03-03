03abf13c28fdb4598522a52e91fee0b40b058029
Jan Midtgaard <mail@janmidtgaard.dk>
2025-03-03 15:18:52 +0100

    Switch to %g format specifier

--- test/core/QCheck_expect_test.ml
+++ test/core/QCheck_expect_test.ml
@@ -1,12 +1,11 @@
 let print_float i64 =
-  Printf.printf "bits: %Li - float: %.2f\n%!" i64 (Int64.to_float i64)
+  Printf.printf "bits: %Li - float: %g\n%!" i64 (Int64.to_float i64)
 
 let _ = List.iter print_float
     [
       4879367852577116075L;
       4880329516726610144L;
       4882384430223028138L;
-      4882522499579827044L;
       4883095049439037711L;
       4883176358557700215L;
     ]

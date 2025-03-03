059f8c68db64d0565eed43181a384e433c269caf
Jan Midtgaard <mail@janmidtgaard.dk>
2025-03-03 14:12:15 +0100

    Reduce test counts

--- test/core/QCheck_tests.ml
+++ test/core/QCheck_tests.ml
@@ -6,12 +6,12 @@ module Stats = struct
 
   let int_dist_tests =
     let dist = ("dist",fun x -> x) in
-    [ Test.make ~name:"int dist"                       ~count:100000 (add_stat dist int)                              (fun _ -> true);
-      Test.make ~name:"oneof int dist"                 ~count:1000   (add_stat dist (oneofl[min_int;-1;0;1;max_int])) (fun _ -> true);
+    [ Test.make ~name:"int dist"                       ~count:100 (add_stat dist int)                              (fun _ -> true);
+      Test.make ~name:"oneof int dist"                 ~count:100   (add_stat dist (oneofl[min_int;-1;0;1;max_int])) (fun _ -> true);
     ]
 
   let int_dist_empty_bucket =
-    Test.make ~name:"int_dist_empty_bucket" ~count:1_000
+    Test.make ~name:"int_dist_empty_bucket" ~count:100
       (add_stat ("dist",fun x -> x) (oneof [small_int_corners ();int])) (fun _ -> true)
 
   let tests = int_dist_tests

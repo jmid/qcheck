efd46213bcf988bba086c91905fb02c275ba17f5
Jan Midtgaard <mail@janmidtgaard.dk>
2025-03-03 13:59:13 +0100

    Reduce QCheck expect tests

--- test/core/QCheck_expect_test.ml
+++ test/core/QCheck_expect_test.ml
@@ -5,11 +5,6 @@ open QCheck_tests
 let () = QCheck_base_runner.set_seed 1234
 let _ =
   QCheck_base_runner.run_tests ~colors:false (
-    Overall.tests @
-    Generator.tests @
-    Shrink.tests @
-    Function.tests @
-    FindExample.tests @
     Stats.tests)
 
 let () = QCheck_base_runner.set_seed 153870556
